2a0113ded28300d435fe2f171b76389e2fced827
devandref <yoandredepaula@gmail.com>
2024-08-07 21:34:21 -0300

    include: testes de integração

--- src/main/java/io/github/devandref/library_events_producer/controller/LibraryEventsController.java
+++ src/main/java/io/github/devandref/library_events_producer/controller/LibraryEventsController.java
@@ -22,7 +22,7 @@ public class LibraryEventsController {
 
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
-        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
+        libraryEventsProducer.sendLibraryEvent_approach2(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
--- src/test/java/io/github/devandref/library_events_producer/controller/LibraryEventsControllerIntegrationTest.java
+++ src/test/java/io/github/devandref/library_events_producer/controller/LibraryEventsControllerIntegrationTest.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
+@EmbeddedKafka(topics = "library-events")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LibraryEventsControllerIntegrationTest {
 
